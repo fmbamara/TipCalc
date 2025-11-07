996853da1b93ed47f5335d1dbb5676975c2728c1
fmbamara <fmbamara@byupathway.edu>
2025-11-07 03:48:21 +0100

    Add plugins for Android and Kotlin in build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    // Standard Android Application Plugin
+    alias(libs.plugins.androidApplication) apply false
+    // Kotlin plugin for the Kotlin language
+    alias(libs.plugins.kotlinAndroid) apply false
+}
+
