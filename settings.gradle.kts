c73975836835203ae5943f3d2c9baad5f65db3f1
fmbamara <fmbamara@byupathway.edu>
2025-11-07 03:46:55 +0100

    Initialize settings.gradle.kts for TipCalc project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "TipCalc"
+include(":app")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
